83b19ffacec76c545e93f679f23c6b88aa11a6d4
a3a <94882349+haruki-0408@users.noreply.github.com>
2026-02-20 18:49:30 +0900

    Update deploy.sh

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,11 +2,11 @@
 FUNCTION_NAME="demo-durable-function"
 
 # Durable Functionsのアップデート
-zip -q function.zip lambda_function.py
+zip -qj function.zip src/lambda_function.py
 aws lambda update-function-code \
   --function-name "$FUNCTION_NAME" \
   --zip-file fileb://function.zip
-rm -rf function.zip
+rm -f function.zip
 
 # バージョンの発行
 aws lambda publish-version \
